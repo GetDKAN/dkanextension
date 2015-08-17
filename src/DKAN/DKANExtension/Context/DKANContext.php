cd2b73dc8458c1aee77507e4db7829b030085843
Aaron Couch <acinternets@gmail.com>
2015-08-16 20:19:42 -0500

    Create DKANContext.php

---
+++ src/DKAN/DKANExtension/Context/DKANContext.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace DKAN\DKANExtension\Context
+
+use Behat\Behat\Context\TranslatableContext;
+use Behat\Mink\Element\Element;
+use Drupal\DrupalExtension\Context
+
+use Behat\Gherkin\Node\TableNode;
+
+/**
+ * Provides pre-built step definitions for interacting with Drupal.
+ */
+class DKANContext extends RawDrupalContext implements TranslatableContext {
+}
