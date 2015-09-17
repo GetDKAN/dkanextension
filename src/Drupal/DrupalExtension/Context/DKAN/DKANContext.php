c1b980b7aed845a708a25af499547e60d8afd2af
Frank Carey <frank@zivtech.com>
2015-09-16 22:28:29 -0400

    Start changing this into a regular context.

--- src/Drupal/DrupalExtension/Context/DKAN/DKANContext.php
+++ src/Drupal/DrupalExtension/Context/DKAN/DKANContext.php
@@ -1,9 +1,219 @@
 <?php
-namespace Drupal\DrupalExtension\Context\DKAN;
+namespace Drupal\DkanExtension\Context;
 
-class DKANContext extends RawDKANContext
-{
+use Drupal\DrupalExtension\Context\RawDrupalContext;
+use Drupal\DrupalExtension\Context\DrupalContext;
+use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat\Behat\Hook\Scope\BeforeStepScope;
+use Behat\Behat\Hook\Scope\AfterStepScope;
+use Behat\Behat\Hook\Scope\BeforeScenarioScope;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+use Behat\Mink\Exception\DriverException;
+use Behat\Behat\Tester\Exception\PendingException;
+
 /**
+ * Defines application features from the specific context.
+ */
+class DKANContext extends RawDrupalContext implements SnippetAcceptingContext {
+
+  // Store pages to be referenced in an array.
+  protected $pages = array();
+  protected $groups = array();
+
+  /**
+   * Initializes context.
+   *
+   * Every scenario gets its own context instance.
+   * You can also pass arbitrary arguments to the
+   * context constructor through behat.yml.
+   */
+  public function __construct() {
+    // Set the default timezone to NY
+    date_default_timezone_set('America/New_York');
+  }
+
+  /******************************
+   * HOOKS
+   ******************************/
+
+  /**
+   * @AfterStep
+   */
+  public function debugStepsAfter(AfterStepScope $scope)
+  {
+    // Tests tagged with @debugEach will perform each step and wait for [ENTER] to proceed.
+    if ($this->scenario->hasTag('debugEach')) {
+      $env = $scope->getEnvironment();
+      $drupalContext = $env->getContext('Drupal\DrupalExtension\Context\DrupalContext');
+      $minkContext = $env->getContext('Drupal\DrupalExtension\Context\MinkContext');
+      // Print the current URL.
+      try {
+        $minkContext->printCurrentUrl();
+      }
+      catch(Behat\Mink\Exception\DriverException $e) {
+        print "No Url";
+      }
+      $drupalContext->iPutABreakpoint();
+    }
+  }
+
+  /**
+   * @BeforeStep
+   */
+  public function debugStepsBefore(BeforeStepScope $scope)
+  {
+    // Tests tagged with @debugBeforeEach will wait for [ENTER] before running each step.
+    if ($this->scenario->hasTag('debugBeforeEach')) {
+      $env = $scope->getEnvironment();
+      $drupalContext = $env->getContext('Drupal\DrupalExtension\Context\DrupalContext');
+      $drupalContext->iPutABreakpoint();
+    }
+  }
+
+  /**
+   * @BeforeScenario
+   */
+  public function registerScenario(BeforeScenarioScope $scope) {
+    // Scenario not usually available to steps, so we do ourselves.
+    // See issue
+    $this->scenario = $scope->getScenario();
+    //print  $this->scenario->getTitle();
+  }
+
+  /**
+   * @BeforeScenario @mail
+   */
+  public function beforeMail()
+  {
+    // Store the original system to restore after the scenario.
+    echo("Setting Testing Mail System\n");
+    $this->originalMailSystem = variable_get('mail_system', array('default-system' => 'DefaultMailSystem'));
+    // Set the test system.
+    variable_set('mail_system', array('default-system' => 'TestingMailSystem'));
+    // Flush the email buffer.
+    variable_set('drupal_test_email_collector', array());
+  }
+
+  /**
+   * @AfterScenario @mail
+   */
+  public function afterMail()
+  {
+    echo("Restoring Mail System\n");
+    // Restore the default system.
+    variable_set('mail_system', $this->originalMailSystem);
+    // Flush the email buffer.
+    variable_set('drupal_test_email_collector', array());
+  }
+
+  /****************************
+   * HELPER FUNCTIONS
+   ****************************/
+
+  /**
+   * Add page to context.
+   *
+   * @param $page
+   */
+  public function addPage($page) {
+    $this->pages[$page['title']] = $page;
+  }
+
+  /**
+   * Get Group by name
+   *
+   * @param $name
+   * @return Group or FALSE
+   */
+  private function getGroupByName($name) {
+    foreach($this->groups as $group) {
+      if ($group->title == $name) {
+        return $group;
+      }
+    }
+    return FALSE;
+  }
+
+  /**
+   * Get Group Role ID by name
+   *
+   * @param $name
+   * @return Group Role ID or FALSE
+   */
+  private function getGroupRoleByName($name) {
+
+    $group_roles = og_get_user_roles_name();
+
+    return array_search($name, $group_roles);
+  }
+
+  /**
+   * Get Membership Status Code by name
+   *
+   * @param $name
+   * @return Membership status code or FALSE
+   */
+  private function getMembershipStatusByName($name) {
+    switch($name) {
+      case 'Active':
+        return OG_STATE_ACTIVE;
+        break;
+      case 'Pending':
+        return OG_STATE_PENDING;
+        break;
+      case 'Blocked':
+        return OG_STATE_BLOCKED;
+        break;
+      default:
+        break;
+    }
+
+    return FALSE;
+  }
+
+  /**
+   * Explode a comma separated string in a standard way.
+   *
+   */
+  function explode_list($string) {
+    $array = explode(',', $string);
+    $array = array_map('trim', $array);
+    return is_array($array) ? $array : array();
+  }
+
+  /**
+   * Get dataset nid by title from context.
+   *
+   * @param $nodeTitle title of the node.
+   * @param $type type of nodo look for.
+   *
+   * @return Node ID or FALSE
+   */
+  private function getNidByTitle($nodeTitle, $type)
+  {
+    $context = array();
+    switch($type) {
+      case 'dataset':
+        $context = $this->datasets;
+        break;
+      case 'resource':
+        $context = $this->resources;
+    }
+
+    foreach($context as $key => $node) {
+      if($node->title == $nodeTitle) {
+        return $key;
+      }
+    }
+    return FALSE;
+  }
+
+  /*****************************
+   * CUSTOM STEPS
+   *****************************/
+
+  /**
    * @Given pages:
    */
   public function addPages(TableNode $pagesTable) {
@@ -12,6 +222,7 @@ class DKANContext extends RawDKANContext
       $this->addPage($pageHash);
     }
   }
+
   /**
    * @Given I am on (the) :page page
    */
@@ -22,6 +233,7 @@ class DKANContext extends RawDKANContext
       $url = $this->pages[$page_title]['url'];
       $session->visit($this->locatePath($url));
       $code = $session->getStatusCode();
+
       if ($code < 200 || $code >= 300) {
         throw new Exception("Page $page_title ($url) visited, but it returned a non-2XX response code of $code.");
       }
@@ -29,7 +241,9 @@ class DKANContext extends RawDKANContext
     else {
       throw new Exception("Page $page_title not found in the pages array, was it added?");
     }
+
   }
+
   /**
    * @When I search for :term
    */
@@ -53,6 +267,7 @@ class DKANContext extends RawDKANContext
       throw new Exception("No search form with the id of found on the page.");
     }
   }
+
   /**
    * @Then I should see a dataset called :text
    *
@@ -65,17 +280,22 @@ class DKANContext extends RawDKANContext
     $page = $session->getPage();
     $search_region = $page->find('css', '.view-dkan-datasets');
     $search_results = $search_region->findAll('css', '.views-row');
+
     $found = false;
     foreach( $search_results as $search_result ) {
+
       $title = $search_result->find('css', 'h2');
+
       if ($title->getText() === $text) {
         $found = true;
       }
     }
+
     if (!$found) {
       throw new \Exception(sprintf("The text '%s' was not found", $text));
     }
   }
+
   /**
    * @Given groups:
    */
@@ -87,6 +307,7 @@ class DKANContext extends RawDKANContext
       'title' => 'title',
       'published' => 'published'
     );
+
     foreach ($groupsTable as $groupHash) {
       $node = new stdClass();
       $node->type = 'group';
@@ -100,8 +321,10 @@ class DKANContext extends RawDKANContext
         }
       }
       $created_node = $this->getDriver()->createNode($node);
+
       // Add the created node to the groups array.
       $this->groups[$created_node->nid] = $created_node;
+
       // Add the url to the page array for easy navigation.
       $this->addPage(array(
         'title' => $created_node->title,
@@ -109,6 +332,7 @@ class DKANContext extends RawDKANContext
       ));
     }
   }
+
   /**
    * Creates multiple group memberships.
    *
@@ -122,11 +346,15 @@ class DKANContext extends RawDKANContext
   public function addGroupMemberships(TableNode $groupMembershipsTable)
   {
     foreach ($groupMembershipsTable->getHash() as $groupMembershipHash) {
+
       if (isset($groupMembershipHash['group']) && isset($groupMembershipHash['user'])) {
+
         $group = $this->getGroupByName($groupMembershipHash['group']);
         $user = user_load_by_name($groupMembershipHash['user']);
+
         // Add user to group with the proper group permissions and status
         if ($group && $user) {
+
           // Add the user to the group
           og_group("node", $group->nid, array(
             "entity type" => "user",
@@ -134,9 +362,11 @@ class DKANContext extends RawDKANContext
             "membership type" => OG_MEMBERSHIP_TYPE_DEFAULT,
             "state" => $this->getMembershipStatusByName($groupMembershipHash['membership status'])
           ));
+
           // Grant user roles
           $group_role = $this->getGroupRoleByName($groupMembershipHash['role on group']);
           og_role_grant("node", $group->nid, $user->uid, $group_role);
+
         } else {
           if (!$group) {
             throw new Exception(sprintf("No group was found with name %s.", $groupMembershipHash['group']));
@@ -150,41 +380,90 @@ class DKANContext extends RawDKANContext
       }
     }
   }
+
   /**
    * @Given datasets:
    */
-  public function addDatasets(TableNode $datasetsTable)
-  {
+  public function addDatasets(TableNode $datasetsTable) {
     // Map readable field names to drupal field names.
     $field_map = array(
       'author' => 'author',
       'title' => 'title',
+      'author' => 'uid',
       'description' => 'body',
-      'publisher' => 'og_group_ref',
-      'published' => 'published',
-      'resource format' => 'resource format',
+      'language' => 'language',
       'tags' => 'field_tags',
+      'publisher' => 'og_group_ref',
+      'moderation' => 'workbench_moderation',
+      'date' => 'created',
     );
-    foreach ($datasetsTable as $groupHash) {
+
+    // Default to draft moderation state.
+    $workbench_moderation_state = 'draft';
+
+    foreach ($datasetsTable as $datasetHash) {
       $node = new stdClass();
-      $node->type = 'group';
-      foreach($groupHash as $field => $value) {
-        if(isset($field_map[$field])) {
-          switch ($field) {
-            case 'tags':
-            case 'publisher':
-              $value = $this->explode_list($value);
-          }
-          $drupal_field = $field_map[$field];
-          $node->$drupal_field = $value;
+
+      // Defaults
+      $node->type = 'dataset';
+      $node->language = LANGUAGE_NONE;
+      $node->is_new = TRUE;
+
+      foreach ($datasetHash as $key => $value) {
+        if (!isset($field_map[$key])) {
+          throw new Exception(sprintf("Dataset's field %s doesn't exist, or hasn't been mapped. See FeatureContext::addDatasets for mappings.", $key));
         }
         else {
-          throw new Exception(sprintf("Dataset's field %s doesn't exist, or hasn't been mapped. See FeatureContext::addDatasets for mappings.", $field));
+          if ($key == 'author') {
+            $user = user_load_by_name($value);
+            if ($user) {
+              $drupal_field = $field_map[$key];
+              $node->$drupal_field = $user->uid;
+            }
+
+          }
+          else {
+            if ($key == 'tags' || $key == 'publisher') {
+              $value = $this->explode_list($value);
+              $drupal_field = $field_map[$key];
+              $node->$drupal_field = $value;
+
+            }
+            else {
+              if ($key == 'moderation') {
+                $workbench_moderation_state = $value;
+
+              }
+              else {
+                // Defalut behavior, map stait to field map.
+                $drupal_field = $field_map[$key];
+                $node->$drupal_field = $value;
+              }
+            }
+          }
         }
       }
+
       $created_node = $this->getDriver()->createNode($node);
+
+      // Make the node author as the revision author.
+      // This is needed for workbench views filtering.
+      $created_node->log = $created_node->uid;
+      $created_node->revision_uid = $created_node->uid;
+      db_update('node_revision')
+        ->fields(array(
+          'uid' => $created_node->uid,
+        ))
+        ->condition('nid', $created_node->nid, '=')
+        ->execute();
+
+      // Manage moderation state.
+      // Requires this patch https://www.drupal.org/node/2393771
+      workbench_moderation_moderate($created_node, $workbench_moderation_state, $created_node->uid);
+
       // Add the created node to the datasets array.
       $this->datasets[$created_node->nid] = $created_node;
+
       // Add the url to the page array for easy navigation.
       $this->addPage(array(
         'title' => $created_node->title,
@@ -193,4 +472,93 @@ class DKANContext extends RawDKANContext
     }
   }
 
+  /**
+   * @Given resources:
+   */
+  public function addResources(TableNode $resourcesTable)
+  {
+    // Map readable field names to drupal field names.
+    $field_map = array(
+      'title' => 'title',
+      'description' => 'body',
+      'author' => 'uid',
+      'language' => 'language',
+      'format' => 'field_format',
+      'dataset' => 'field_dataset_ref',
+      'date' => 'created',
+      'moderation' => 'workbench_moderation',
+    );
+
+    // Default to draft moderation state.
+    $workbench_moderation_state = 'draft';
+
+    foreach ($resourcesTable as $resourceHash) {
+      $node = new stdClass();
+      $node->type = 'resource';
+
+      // Defaults
+      $node->language = LANGUAGE_NONE;
+
+      foreach($resourceHash as $key => $value) {
+        $drupal_field = $field_map[$key];
+
+        if(!isset($field_map[$key])) {
+          throw new Exception(sprintf("Resource's field %s doesn't exist, or hasn't been mapped. See FeatureContext::addDatasets for mappings.", $key));
+
+        } else if($key == 'author') {
+          $user = user_load_by_name($value);
+          if(!isset($user)) {
+            $value = $user->uid;
+          }
+          $drupal_field = $field_map[$key];
+          $node->$drupal_field = $value;
+
+        } elseif ($key == 'format') {
+          $value = $this->explode_list($value);
+          $node->{$drupal_field} = $value;
+
+        } elseif ($key == 'dataset') {
+          if( $nid = $this->getNidByTitle($value, 'dataset')) {
+            $node->{$drupal_field}['und'][0]['target_id'] = $nid;
+          }else {
+            throw new Exception(sprintf("Dataset node not found."));
+          }
+
+        } else if($key == 'moderation') {
+          // No need to define 'Draft' state as it is used as default.
+          $workbench_moderation_state = $value;
+
+        } else {
+          // Default behavior.
+          // PHP 5.4 supported notation.
+          $node->{$drupal_field} = $value;
+        }
+      }
+
+      $created_node = $this->getDriver()->createNode($node);
+
+      // Make the node author as the revision author.
+      // This is needed for workbench views filtering.
+      $created_node->log = $created_node->uid;
+      $created_node->revision_uid = $created_node->uid;
+      db_update('node_revision')
+        ->fields(array(
+          'uid' => $created_node->uid,
+        ))
+        ->condition('nid', $created_node->nid, '=')
+        ->execute();
+
+      // Manage moderation state.
+      workbench_moderation_moderate($created_node, $workbench_moderation_state);
+
+      // Add the created node to the datasets array.
+      $this->resources[$created_node->nid] = $created_node;
+
+      // Add the url to the page array for easy navigation.
+      $this->addPage(array(
+        'title' => $created_node->title,
+        'url' => '/node/' . $created_node->nid
+      ));
+    }
+  }
 }
