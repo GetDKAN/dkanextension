065b587e04febc69c433dd67612dea374a685004
Frank Carey <frank@devinci.io>
2015-10-05 13:41:11 -0400

    Recreate a GroupContext from RawDKANEntityContext and move the Group specific methods from RawDKANContext into it. (note this hasn't been tested as working locally yet.)

--- src/Drupal/DKANExtension/Context/RawDKANContext.php
+++ src/Drupal/DKANExtension/Context/RawDKANContext.php
@@ -15,10 +15,6 @@ use \stdClass;
  */
 class RawDKANContext extends RawDrupalContext implements SnippetAcceptingContext {
 
-  // Store pages to be referenced in an array.
-  protected $pages = array();
-  protected $groups = array();
-
   /**
    * Initializes context.
    *
@@ -29,82 +25,6 @@ class RawDKANContext extends RawDrupalContext implements SnippetAcceptingContext
   public function __construct() {
     // Set the default timezone to NY
     date_default_timezone_set('America/New_York');
-  }
-
-
-  /****************************
-   * HELPER FUNCTIONS
-   ****************************/
-
-  /**
-   * Add page to context.
-   *
-   * @param $page
-   */
-  public function addPage($page) {
-    $this->pages[$page['title']] = $page;
-  }
-
-  /**
-   * Get Group by name
-   *
-   * @param $name
-   * @return Group or FALSE
-   */
-  private function getGroupByName($name) {
-    foreach ($this->groups as $group) {
-      if ($group->title == $name) {
-        return $group;
-      }
-    }
-    return FALSE;
-  }
-
-  /**
-   * Get Group Role ID by name
-   *
-   * @param $name
-   * @return Group Role ID or FALSE
-   */
-  private function getGroupRoleByName($name) {
-
-    $group_roles = og_get_user_roles_name();
-
-    return array_search($name, $group_roles);
-  }
-
-  /**
-   * Get Membership Status Code by name
-   *
-   * @param $name
-   * @return Membership status code or FALSE
-   */
-  private function getMembershipStatusByName($name) {
-    switch ($name) {
-      case 'Active':
-        return OG_STATE_ACTIVE;
-        break;
-      case 'Pending':
-        return OG_STATE_PENDING;
-        break;
-      case 'Blocked':
-        return OG_STATE_BLOCKED;
-        break;
-      default:
-        break;
-    }
-
-    return FALSE;
-  }
-
-  /**
-   * Explode a comma separated string in a standard way.
-   *
-   */
-  function explode_list($string) {
-    $array = explode(',', $string);
-    $array = array_map('trim', $array);
-    return is_array($array) ? $array : array();
   }
 
   /**
