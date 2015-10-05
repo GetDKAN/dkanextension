af400424dcb63027b0ffa3ea77622cc54769ef18
Frank Carey <frank@devinci.io>
2015-10-05 13:13:03 -0400

    Convert remaining items to RawDKANContext, not sure if we'll keep this or not.

---
+++ src/Drupal/DKANExtension/Context/RawDKANContext.php
@@ -0,0 +1,135 @@
+<?php
+namespace Drupal\DKANExtension\Context;
+
+use Drupal\DrupalExtension\Context\RawDrupalContext;
+use Drupal\DrupalExtension\Context\DrupalContext;
+use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat\Mink\Exception\UnsupportedDriverActionException as UnsupportedDriverActionException;
+use Behat\Gherkin\Node\TableNode;
+use Behat\Mink\Exception\DriverException;
+use Behat\Behat\Tester\Exception\PendingException;
+use \stdClass;
+
+/**
+ * Defines application features from the specific context.
+ */
+class RawDKANContext extends RawDrupalContext implements SnippetAcceptingContext {
+
+  // Store pages to be referenced in an array.
+  protected $pages = array();
+  protected $groups = array();
+
+  /**
+   * Initializes context.
+   *
+   * Every scenario gets its own context instance.
+   * You can also pass arbitrary arguments to the
+   * context constructor through behat.yml.
+   */
+  public function __construct() {
+    // Set the default timezone to NY
+    date_default_timezone_set('America/New_York');
+  }
+
+
+  /****************************
+   * HELPER FUNCTIONS
+   ****************************/
+
+  /**
+   * Add page to context.
+   *
+   * @param $page
+   */
+  public function addPage($page) {
+    $this->pages[$page['title']] = $page;
+  }
+
+  /**
+   * Get Group by name
+   *
+   * @param $name
+   * @return Group or FALSE
+   */
+  private function getGroupByName($name) {
+    foreach ($this->groups as $group) {
+      if ($group->title == $name) {
+        return $group;
+      }
+    }
+    return FALSE;
+  }
+
+  /**
+   * Get Group Role ID by name
+   *
+   * @param $name
+   * @return Group Role ID or FALSE
+   */
+  private function getGroupRoleByName($name) {
+
+    $group_roles = og_get_user_roles_name();
+
+    return array_search($name, $group_roles);
+  }
+
+  /**
+   * Get Membership Status Code by name
+   *
+   * @param $name
+   * @return Membership status code or FALSE
+   */
+  private function getMembershipStatusByName($name) {
+    switch ($name) {
+      case 'Active':
+        return OG_STATE_ACTIVE;
+        break;
+      case 'Pending':
+        return OG_STATE_PENDING;
+        break;
+      case 'Blocked':
+        return OG_STATE_BLOCKED;
+        break;
+      default:
+        break;
+    }
+
+    return FALSE;
+  }
+
+  /**
+   * Explode a comma separated string in a standard way.
+   *
+   */
+  function explode_list($string) {
+    $array = explode(',', $string);
+    $array = array_map('trim', $array);
+    return is_array($array) ? $array : array();
+  }
+
+  /**
+   * Get dataset nid by title from context.
+   *
+   * @param $nodeTitle title of the node.
+   * @param $type type of nodo look for.
+   *
+   * @return Node ID or FALSE
+   */
+  private function getNidByTitle($nodeTitle, $type) {
+    $context = array();
+    switch ($type) {
+      case 'dataset':
+        $context = $this->datasets;
+        break;
+      case 'resource':
+        $context = $this->resources;
+    }
+
+    foreach ($context as $key => $node) {
+      if ($node->title == $nodeTitle) {
+        return $key;
+      }
+    }
+    return FALSE;
+  }
+}
