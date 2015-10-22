b302aa4c1e1a90ae45d44c13ac28d6263b7fc089
keelahnkhan <kpt9408@rit.edu>
2015-10-22 15:00:43 -0400

    Fixup RawDKANEntityContext so it properly saves and deletes entities. Tested as working with GroupContext.

--- src/Drupal/DKANExtension/Context/RawDKANEntityContext.php
+++ src/Drupal/DKANExtension/Context/RawDKANEntityContext.php
@@ -46,7 +46,7 @@ class RawDKANEntityContext extends RawDrupalContext implements SnippetAcceptingC
   public function deleteAll(AfterScenarioScope $scope) {
     foreach ($this->entities as $entity) {
       $ids = entity_extract_ids($this->entity_type, $entity);
-      entity_delete($this->entity_type, entity_id($this->entity_type, $ids[0]));
+      entity_delete($this->entity_type, $ids[0]);
     }
   }
 
@@ -76,11 +76,11 @@ class RawDKANEntityContext extends RawDrupalContext implements SnippetAcceptingC
   }
 
   public function create($entity) {
-    return entity_create($this->entity_type, $entity);
+    return entity_create($this->entity_type, (array) $entity);
   }
 
   public function save($entity) {
-    $entity = entity_save($this->entity_type, $entity);
+    entity_save($this->entity_type, $entity);
 
     list($id, $vid, $bundle) = entity_extract_ids($this->entity_type, $entity);
     // Add the created entity to the array so it can be deleted later.
@@ -107,7 +107,7 @@ class RawDKANEntityContext extends RawDrupalContext implements SnippetAcceptingC
   }
 
   public function add($entity) {
-    $this->create($entity);
+    $entity = $this->create($entity);
     $this->save($entity);
   }
 
@@ -124,6 +124,12 @@ class RawDKANEntityContext extends RawDrupalContext implements SnippetAcceptingC
           throw new \Exception(sprintf("Entity field %s doesn't exist, or hasn't been mapped.", $field));
         }
       }
+      // Add additional defaults like "type", and map user id to author.
+      if($this->bundle != NULL) {
+        $entity->type = $this->bundle;
+      }
+      $author = user_load_by_name($entity->author);
+      $entity->author = $author->uid;
       $entities[] = $entity;
     }
     return $entities;
