3e6eb2586afa760f060c5f95d1a5bd459ce0e6df
Frank Carey <frank@devinci.io>
2015-11-16 16:03:07 -0500

    Copy over some email checking functions from data_starter's FeatureContext.

--- src/Drupal/DKANExtension/Context/MailContext.php
+++ src/Drupal/DKANExtension/Context/MailContext.php
@@ -10,6 +10,7 @@ use Behat\Behat\Context\SnippetAcceptingContext;
 class MailContext extends RawDrupalContext implements SnippetAcceptingContext {
 
   public $originalMailSystem =  array('default-system' => 'DefaultMailSystem');
+  protected $activeEmail;
 
   public function __construct() {
     $this->originalMailSystem = variable_get('mail_system', $this->originalMailSystem);
@@ -44,9 +45,76 @@ class MailContext extends RawDrupalContext implements SnippetAcceptingContext {
     variable_set('drupal_test_email_collector', array());
   }
 
-  public function getMail() {
-    variable_get('drupal_test_email_collector', array());
+  public function getMails() {
+    // We can't use variable_get() because $conf is only fetched once per
+    // scenario... (TODO IS THIS TRUE? seems like it should work fine because of variable_set()
+    // setting the database and $conf (settings cache) --Frank)
+    $variables = array_map('unserialize', db_query("SELECT name, value FROM {variable} WHERE name = 'drupal_test_email_collector'")->fetchAllKeyed());
+    if (isset($variables['drupal_test_email_collector'])) {
+      return $variables['drupal_test_email_collector'];
+    }
+    return array();
   }
 
-  //TODO Create steps for testing values of sent mails.
+  /**
+   * @Then user :username should receive an email
+   * @Then user :username should receive an email containing :content
+   */
+  public function userShouldReceiveAnEmail($username, $content = '')
+  {
+    $found = false;
+    if($user = user_load_by_name($username)) {
+      foreach ($this->getMails() as $message) {
+        try {
+          $this->assertShouldBeAddressedToEmail($message, $user->mail);
+          $this->assertEmailShouldContain($message, $content);
+          $found = TRUE;
+        } catch (\Exception $e) {
+        }
+      }
+      if (!$found) {
+        throw new \Exception(sprintf("Email to %s with content %s not found.", $username, $content));
+      }
+    else {
+        throw new \Exception(sprintf("User %s not found.", $username));
+      }
+    }
+  }
+
+  /**
+   * @Then the email address :emailAddress should receive an email
+   * @Then the email address :emailAddress should receive an email containing :content
+   */
+  public function emailShouldReceiveAnEmailContaining($emailAddress, $content = '')
+  {
+    $found = false;
+    foreach ($this->getMails() as $message) {
+      try {
+        $this->assertShouldBeAddressedToEmail($message, $emailAddress);
+        $this->assertEmailShouldContain($message, $content);
+        $found = TRUE;
+      } catch (\Exception $e) {
+      }
+    }
+    if (!$found) {
+      throw new \Exception(sprintf("Email to %s with content %s not found.", $emailAddress, $content));
+    }
+  }
+
+  public function assertShouldBeAddressedToEmail($message, $email_addr) {
+    if (!isset($message['to']) || $message['to'] !== $email_addr) {
+      throw new \Exception(sprintf('Email to %s not found', $email_addr));
+    }
+  }
+
+  public function assertEmailShouldContain($message, $content = '') {
+      if (empty($content)) {
+        return TRUE;
+      }
+      elseif (strpos($message['body'], $content) !== FALSE ||
+        strpos($message['subject'], $content) !== FALSE) {
+        return TRUE;
+      }
+      throw new \Exception('Did not find expected content in message body or subject.');
+  }
 }
