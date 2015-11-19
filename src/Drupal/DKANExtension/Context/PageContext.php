18b4213871935891200eed0fbd2c2aa6636d4f53
Achraf Jeday <achraf.jday@gmail.com>
2015-11-19 17:32:27 +0100

    Add steps for node access.
    Steps added:
    * @Given I should be able to edit :page
    * @Given I should not be able to edit :page
    * @Given I should be able to delete :page
    * @Given I should not be able to delete :page

--- src/Drupal/DKANExtension/Context/PageContext.php
+++ src/Drupal/DKANExtension/Context/PageContext.php
@@ -5,6 +5,7 @@ use Behat\Behat\Context\Context as Context;
 use Behat\Mink\Exception\UnsupportedDriverActionException as UnsupportedDriverActionException;
 use Behat\Gherkin\Node\TableNode;
 use Drupal\DrupalExtension\Context\RawDrupalContext;
+use EntityFieldQuery;
 
 /**
  * Defines application features from the specific context.
@@ -13,6 +14,27 @@ class PageContext extends RawDrupalContext {
 
   // Store pages to be referenced in an array.
   protected $pages = array();
+
+  /**
+   * Get node by title from Database.
+   *
+   * @param $title: title of the node.
+   * @param $reset: reset drupal cache before looking for the node.
+   *
+   * @return Node ID or FALSE
+   */
+  function getNodeByTitle($title) {
+    $query = new EntityFieldQuery();
+    $query->entityCondition('entity_type', 'node')
+      ->propertyCondition('title', $title)
+      ->range(0, 1);
+    $result = $query->execute();
+    if (isset($result['node'])) {
+      $nid = array_keys($result['node']);
+      return entity_load('node', $nid);
+    }
+    return false;
+  }
 
   /**
    * Add page to context.
@@ -55,4 +77,78 @@ class PageContext extends RawDrupalContext {
       throw new \Exception("Page $page_title not found in the pages array, was it added?");
     }
   }
+
+  /**
+   * @Given I should be able to edit :page
+   */
+  public function iShouldBeAbleToEditPage($page) {
+    $node = $this->getNodeByTitle($page);
+    if(!$node) {
+      throw new \Exception(sprintf($page . " node not found."));
+    }
+
+    $session = $this->getSession();
+    $url = "/node/" . $node->nid . "/edit";
+    $session->visit($this->locatePath($url));
+    $code = $session->getStatusCode();
+    if ($code == 403) {
+      throw new \Exception("403 Forbidden: the server refused to respond.");
+    }
+  }
+
+  /**
+   * @Given I should not be able to edit :page
+   */
+  public function iShouldNotBeAbleToEditPage($page) {
+    $node = $this->getNodeByTitle($page);
+    if(!$node) {
+      throw new \Exception(sprintf($page . " node not found."));
+    }
+
+    $session = $this->getSession();
+    $url = "/node/" . $node->nid . "/edit";
+    $session->visit($this->locatePath($url));
+    $code = $session->getStatusCode();
+    if ($code == 200) {
+      throw new \Exception("200 OK: the page is accessible.");
+    }
+  }
+
+  /**
+   * @Given I should be able to delete :page
+   */
+  public function iShouldBeAbleToDeletePage($page) {
+    $node = $this->getNodeByTitle($page);
+    if(!$node) {
+      throw new \Exception(sprintf($page . " node not found."));
+    }
+
+    $session = $this->getSession();
+    $url = "/node/" . $node->nid . "/delete";
+    $session->visit($this->locatePath($url));
+    $code = $session->getStatusCode();
+    if ($code == 403) {
+      throw new \Exception("403 Forbidden: the server refused to respond.");
+    }
+  }
+
+  /**
+   * @Given I should not be able to delete :page
+   */
+  public function iShouldNotBeAbleToDeletePage($page) {
+    $node = $this->getNidByTitle($page);
+    if(!$node) {
+      throw new \Exception(sprintf($page . " node not found."));
+    }
+
+    $session = $this->getSession();
+    $url = "/node/" . $node->nid . "/delete";
+    $session->visit($this->locatePath($url));
+    $code = $session->getStatusCode();
+
+    if ($code == 200) {
+      throw new \Exception("200 OK: the page is accessible.");
+    }
+  }
+
 }
