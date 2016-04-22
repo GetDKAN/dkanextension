d5c4add2b56bd24212f2d88e3ff1c31c2546b835
Sol Villar <msol.villar@gmail.com>
2016-04-21 23:17:37 -0300

    Updated pod test.

--- src/Drupal/DKANExtension/Context/PODContext.php
+++ src/Drupal/DKANExtension/Context/PODContext.php
@@ -1,15 +1,28 @@
 <?php
 namespace Drupal\DKANExtension\Context;
 
+use Drupal\DKANExtension\Context\PageContext;
+
 
 /**
  * Defines application features from the specific context.
  */
 class PODContext extends RawDKANContext {
+
+  /**
+   * @BeforeScenario
+   */
+ public function gatherContexts(BeforeScenarioScope $scope){
+   parent::gatherContexts($scope);
+   $environment = $scope->getEnvironment();
+   $this->pageContext = $environment->getContext('Drupal\DKANExtension\Context\PageContext');
+ }
+
   /**
    * @When I should see valid data.json on the /data.json page
    */
   public function iShouldSeeValidDatasjonOnTheDatajsonPage() {
+    $this->pageContext->visitPage('/data.json');
     $results = open_data_schema_pod_process_validate($this->getMinkParameter('base_url') . '/data.json', TRUE);
     if ($results['errors']) {
       throw new \Exception(sprintf('Data.json is not valid.'));
