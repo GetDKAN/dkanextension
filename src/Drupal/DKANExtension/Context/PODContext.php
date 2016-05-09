0818560913105ce21f82f3d94f91f298f74c5d19
Sol Villar <msol.villar@gmail.com>
2016-05-09 13:34:57 -0300

    Removed temporal log.

--- src/Drupal/DKANExtension/Context/PODContext.php
+++ src/Drupal/DKANExtension/Context/PODContext.php
@@ -23,9 +23,6 @@ class PODContext extends RawDKANContext {
    * @When I should see valid data.json
    */
   public function iShouldSeeValidDatasjon() {
-    $page_content = $this->getSession()->getDriver()->getContent();
-    var_dump($page_content);
-
     $results = open_data_schema_pod_process_validate($this->getMinkParameter('base_url') . '/data.json', TRUE);
     if ($results['errors']) {
       throw new \Exception(sprintf('Data.json is not valid.'));
