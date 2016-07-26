0c6f482b0d2781b21a551e9faad6931f2a65bb99
msolv <msolv@users.noreply.github.com>
2016-07-26 12:05:16 -0300

    Added missing semicolon.

--- src/Drupal/DKANExtension/Context/PODContext.php
+++ src/Drupal/DKANExtension/Context/PODContext.php
@@ -23,7 +23,7 @@ class PODContext extends RawDKANContext {
    * @When I should see valid data.json
    */
   public function iShouldSeeValidDatasjon() {
-    $url = $this->getMinkParameter('base_url') ? $this->getMinkParameter('base_url') : "http://127.0.0.1::8888"
+    $url = $this->getMinkParameter('base_url') ? $this->getMinkParameter('base_url') : "http://127.0.0.1::8888";
     $results = open_data_schema_pod_process_validate($url . '/data.json', TRUE);
     if ($results['errors']) {
       throw new \Exception(sprintf('Data.json is not valid.'));
