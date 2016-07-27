144881fc42c489ed7834c7a903db62d1312dfff9
Sol Villar <msol.villar@gmail.com>
2016-07-27 11:34:04 -0300

    Added fix for running POD validation tests on CircleCI.

--- src/Drupal/DKANExtension/Context/PODContext.php
+++ src/Drupal/DKANExtension/Context/PODContext.php
@@ -23,8 +23,18 @@ class PODContext extends RawDKANContext {
    * @When I should see valid data.json
    */
   public function iShouldSeeValidDatasjon() {
+    // Change /data.json path to /json during tests. The '.' on the filename breaks tests on CircleCI's server.
+    $data_json = open_data_schema_map_api_load('data_json_1_1');
+    $data_json->endpoint = 'json';
+    drupal_write_record('open_data_schema_map', $data_json, 'id');
+    drupal_static_reset('open_data_schema_map_api_load_all');
+    menu_rebuild();
+
+    // Get base URL.
     $url = $this->getMinkParameter('base_url') ? $this->getMinkParameter('base_url') : "http://127.0.0.1::8888";
-    $results = open_data_schema_pod_process_validate($url . '/data.json', TRUE);
+    
+    // Validate POD.
+    $results = open_data_schema_pod_process_validate($url . '/json', TRUE);
     if ($results['errors']) {
       throw new \Exception(sprintf('Data.json is not valid.'));
     }
