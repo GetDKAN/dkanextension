65a02d14dac9367fa644b27fbc4fb5e6cb435fd3
Sol Villar <msol.villar@gmail.com>
2016-08-05 01:04:10 -0300

    Extended POD Context with ODFE related steps.

--- src/Drupal/DKANExtension/Context/PODContext.php
+++ src/Drupal/DKANExtension/Context/PODContext.php
@@ -2,10 +2,24 @@
 namespace Drupal\DKANExtension\Context;
 use Drupal\DKANExtension\Context\PageContext;
 use Behat\Behat\Hook\Scope\BeforeScenarioScope;
+use Behat\Behat\Hook\Scope\AfterScenarioScope;
+
 /**
  * Defines application features from the specific context.
  */
 class PODContext extends RawDKANContext {
+
+  private $required_pod_fields = array(
+    'title' => 'Title',
+    'description' => 'Description',
+    'keyword' => 'Tags',
+    'contactPoint:email' => 'Contact Email',
+    'contactPoint:name' => 'Contact Name',
+    'accessLevel' => 'Public Access Level',
+    'bureauCodeUSG' => 'Bureau Code',
+    'programCodeUSG ' => 'Program Code',
+  );
+
   /**
    * @BeforeScenario
    */
@@ -20,10 +34,12 @@ class PODContext extends RawDKANContext {
   public function iShouldSeeAValidDatasjon() {
     // Change /data.json path to /json during tests. The '.' on the filename breaks tests on CircleCI's server.
     $data_json = open_data_schema_map_api_load('data_json_1_1');
-    $data_json->endpoint = 'json';
-    drupal_write_record('open_data_schema_map', $data_json, 'id');
-    drupal_static_reset('open_data_schema_map_api_load_all');
-    menu_rebuild();
+    if ($data_json->endpoint !== 'json') {
+      $data_json->endpoint = 'json';
+      drupal_write_record('open_data_schema_map', $data_json, 'id');
+      drupal_static_reset('open_data_schema_map_api_load_all');
+      menu_rebuild();
+    }
     // Get base URL.
     $url = $this->getMinkParameter('base_url') ? $this->getMinkParameter('base_url') : "http://127.0.0.1::8888";
 
@@ -32,5 +48,68 @@ class PODContext extends RawDKANContext {
     if ($results['errors']) {
       throw new \Exception(sprintf('Data.json is not valid.'));
     }
+  }
+
+  /**
+   * @Then I should see all of the Federal Extras fields
+   */
+  public function iShouldSeeAllOfTheFederalExtrasFields()
+  {
+    $feFields = array(
+      'bureauCodeUSG' => 'Bureau Code',
+      'programCodeUSG ' => 'Program Code',
+      'dataQualityUSG' => 'Data Quality',
+      'primaryITInvestmentUIIUSG' => 'Primary IT Investment UII',
+      'systemOfRecordsUSG' => 'System of Records',
+    );
+
+    foreach ($feFields as $key => $fieldName) {
+      $this->assertSession()->pageTextContains($fieldName);
+    }
+  }
+
+  /**
+   * @Then I should see all POD required fields
+   */
+  public function iShouldSeeAllPodRequiredFields()
+  {
+    foreach ($this->required_pod_fields as $key => $fieldName) {
+      $this->assertSession()->pageTextContains($fieldName);
+    }
+  }
+
+  /**
+   * @Then I should see an error for POD required fields
+   */
+  public function iShouldSeeAnErrorForPodRequiredFields()
+  {
+    foreach ($this->required_pod_fields as $key => $fieldName) {
+      $this->assertSession()->pageTextContains($fieldName . ' field is required.');
+    }
+  }
+
+  /**
+   * @BeforeScenario @add_ODFE
+   */
+  public function addODFE(BeforeScenarioScope $event)
+  {
+    // Enable 'open_data_federal_extras' module.
+    module_enable(array('open_data_federal_extras'));
+  }
+
+  /**
+   * @AfterScenario @remove_ODFE
+   */
+  public function removeODFE(AfterScenarioScope $event)
+  {
+    // Disable 'open_data_federal_extras' module.
+    module_disable(array('open_data_federal_extras'));
+
+    // Remove ODFE fields.
+    field_delete_field('field_odfe_bureau_code');
+    field_delete_field('field_odfe_data_quality');
+    field_delete_field('field_odfe_investment_uii');
+    field_delete_field('field_odfe_program_code');
+    field_delete_field('field_odfe_system_of_records');
   }
 }
