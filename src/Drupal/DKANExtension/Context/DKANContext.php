e8024a75b022cc2c5598a958bde39a8ddc4e7f33
dkinzer <dtkinzer@gmail.com>
2016-09-09 16:28:45 -0400

    Remove duplicate context step.

--- src/Drupal/DKANExtension/Context/DKANContext.php
+++ src/Drupal/DKANExtension/Context/DKANContext.php
@@ -696,28 +696,6 @@ public function iWaitForTextToDisappear($text)
   }
 
   /**
-   * Checks if a button with id|name|title|alt|value exists in a region
-   *
-   * @Then I should see the button :button in the :region( region)
-   * @Then I should see the :button button in the :region( region)
-   *
-   * @param $button
-   *   string The id|name|title|alt|value of the button
-   * @param $region
-   *   string The region in which the button should not be found
-   *
-   * @throws \Exception
-   *   If region cannot be found or the button is present on region.
-   */
-  public function iShouldSeeTheButtonInThe($button, $region) {
-    $regionObj = $this->getMink()->getRegion($region);
-    $buttonObj = $regionObj->findButton($button);
-    if (!$buttonObj) {
-      throw new \Exception(sprintf("The button '%s' is not present in the region '%s' on the page %s", $button, $region, $this->getSession()->getCurrentUrl()));
-    }
-  }
-
-  /**
    * @Then all default content with type :type and bundle :bundle listed in :fixture fixture should :status
    */
   public function allDefaultContentWithTypeAndBundleListedInFixtureShould($type, $bundle, $fixture, $status)
