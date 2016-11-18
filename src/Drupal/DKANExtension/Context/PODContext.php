c21e147ab445f7760ec95cdc989f99dc2439bb99
Jacinto capote Robles <jacintocapote@gmail.com>
2016-11-19 00:26:26 +0100

    Added new step should not see a valid data json

--- src/Drupal/DKANExtension/Context/PODContext.php
+++ src/Drupal/DKANExtension/Context/PODContext.php
@@ -51,6 +51,28 @@ class PODContext extends RawDKANContext {
   }
 
   /**
+   * @When I should not see a valid data.json
+   */
+  public function iShouldNotSeeAValidDatasjon() {
+    // Change /data.json path to /json during tests. The '.' on the filename breaks tests on CircleCI's server.
+    $data_json = open_data_schema_map_api_load('data_json_1_1');
+    if ($data_json->endpoint !== 'json') {
+      $data_json->endpoint = 'json';
+      drupal_write_record('open_data_schema_map', $data_json, 'id');
+      drupal_static_reset('open_data_schema_map_api_load_all');
+      menu_rebuild();
+    }
+    // Get base URL.
+    $url = $this->getMinkParameter('base_url') ? $this->getMinkParameter('base_url') : "http://127.0.0.1::8888";
+
+    // Validate POD.
+    $results = open_data_schema_pod_process_validate($url . '/json', TRUE);
+    if (!$results['errors']) {
+      throw new \Exception(sprintf('Data.json is not valid.'));
+    }
+  }
+
+  /**
    * @Then I should see a valid catalog xml
    */
   public function iShouldSeeAValidCatalogXml() {
